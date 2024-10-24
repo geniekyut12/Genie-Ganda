cb4b54d6697faf1ceb3285ffb65b6a47cda47b07
geniekyut12 <estebangenie12@gmail.com>
2024-10-24 17:15:07 +0800

    Update

--- app/src/main/java/com/example/firstpage/Secondpage.java
+++ app/src/main/java/com/example/firstpage/Secondpage.java
@@ -1,13 +1,19 @@
 package com.example.firstpage;
 
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.VideoView;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class Secondpage extends AppCompatActivity {
+
+    private Button btnSignIn;
 
 
     @Override
@@ -15,6 +21,15 @@ public class Secondpage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondpage);
 
+        // next page
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Secondpage.this, Register.class);
+                startActivity(intent);
+            }
+        });
 
         // Find VideoView by its ID
         VideoView videoView = findViewById(R.id.videoViewBackground);
